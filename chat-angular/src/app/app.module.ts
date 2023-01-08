2c1a8ec295026fe76613514cdb05ffd217e3b09f
ThallesRyan <84140766+thallesryan@users.noreply.github.com>
2023-01-08 16:52:10 -0300

    chat component

--- chat-angular/src/app/app.module.ts
+++ chat-angular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { ChatComponent } from './contacts/chat/chat.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
--- chat-angular/src/app/pages/contacts/contacts.module.ts
+++ chat-angular/src/app/pages/contacts/contacts.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { ContactsRoutingModule } from './contacts-routing.module';
 import { ListContactsComponent } from './list-contacts/list-contacts.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ChatComponent } from './chat/chat.component';
 
 
 @NgModule({
   declarations: [
-    ListContactsComponent
+    ListContactsComponent,
+    ChatComponent
   ],
   imports: [
     CommonModule,
